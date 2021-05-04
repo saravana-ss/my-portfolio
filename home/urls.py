018f32ce5d6bda241f6b3444a4a0175589a28c24
saravana-ss <mails2saravana@gmail.com>
2021-05-04 23:53:30 +0530

    basic templates

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('project', views.project, name='project')
+    path('projects', views.projects, name='projects')
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is my homepage(/)")
+    #return render(request, 'home.html')
+    context = {'name' : 'Saravana', 'course' : 'Django'}
+    return render(request, 'home.html', context)
 
 def about(request):
     return HttpResponse("This is my homepage(/)")
@@ -10,5 +12,8 @@ def about(request):
 def contact(request):
     return HttpResponse("This is my homepage(/)")
 
+def projects(request):
+    #return HttpResponse("This is my homepage(/)")
+    return render(request, 'projects.html')
 
     
