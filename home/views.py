c789caba14c2dbd199f0ba506b74451ef49d05b6
saravana-ss <mails2saravana@gmail.com>
2021-05-04 20:51:37 +0530

    First commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("This is my homepage(/)")
+
+def about(request):
+    return HttpResponse("This is my homepage(/)")
+
+def contact(request):
+    return HttpResponse("This is my homepage(/)")
+
+def project(request):
+    return HttpResponse("This is my homepage(/)")
